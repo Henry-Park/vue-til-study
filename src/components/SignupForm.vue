579829207e78bd231f8c0cb0f13a475c92a18024
Henry-Park <psgi0040@gmail.com>
2020-10-01 03:31:47 +0900

    session_6_styling

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,26 +1,29 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<!--  @ == v-on -->
-		<div>
-			<label for="username">id: </label>
-			<input id="username" type="text" v-model="username" />
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm" class="form">
+				<!--  @ == v-on -->
+				<div>
+					<label for="username">id: </label>
+					<input id="username" type="text" v-model="username" />
+				</div>
+				<div>
+					<label for="password">pw: </label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<div>
+					<label for="nickname">nickname: </label>
+					<input id="nickname" type="text" v-model="nickname" />
+				</div>
+				<button type="submit" class="btn">회원 가입</button>
+			</form>
+			<p class="log">{{ logMessage }}</p>
 		</div>
-		<div>
-			<label for="password">pw: </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<div>
-			<label for="nickname">nickname: </label>
-			<input id="nickname" type="text" v-model="nickname" />
-		</div>
-		<button type="submit">회원 가입</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
 import { registerUser } from '@/api/index';
-
 export default {
 	data() {
 		return {
@@ -34,7 +37,7 @@ export default {
 	},
 	methods: {
 		async submitForm() {
-			console.log('폼 제출');
+			// console.log('폼 제출');
 			const userData = {
 				username: this.username,
 				password: this.password,
@@ -42,7 +45,7 @@ export default {
 			};
 			const { data } = await registerUser(userData);
 			console.log(data.username);
-			this.logMessage = `${data.username}님이 가입되었습니다.`;
+			this.logMessage = `${data.username} 님이 가입되었습니다`;
 			this.initForm();
 		},
 		initForm() {
