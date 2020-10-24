ca16209b9027acac161e9aca24e0c59970ef5909
Henry-Park <psgi0040@gmail.com>
2020-10-25 01:58:18 +0900

    session_17

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
+
 // import { component } from 'vue/types/umd';
 // import Home from '../views/Home.vue';
 // import LoginPage from '@/views/LoginPage.vue';
@@ -7,7 +9,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: 'history', //url의 #을 제거하기 위함
 	routes: [
 		{
@@ -25,14 +27,17 @@ export default new VueRouter({
 		{
 			path: '/main',
 			component: () => import('@/views/MainPage.vue'),
+			meta: { auth: true },
 		},
 		{
 			path: '/add',
 			component: () => import('@/views/PostAddPage.vue'),
+			meta: { auth: true },
 		},
 		{
 			path: '/post/:id',
 			component: () => import('@/views/PostEditPage.vue'),
+			meta: { auth: true },
 		},
 		{
 			path: '*',
@@ -41,6 +46,17 @@ export default new VueRouter({
 	],
 });
 
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		console.log('인증이 필요합니다.');
+		next('/login');
+		return;
+	}
+	next();
+});
+
+// to : 이동하려는 페이지   from : 현재페이지   next : 페이지 이동할 때 호출하는 API
+export default router;
 // const routes = [
 // 	{
 // 		path: '/',
